7c16d372d8534b16219a6f2049fba9bc7f8e64d9
Jessie Guzman <jessieguzman777@gmail.com>
2022-11-06 15:35:49 -0600

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(arrayStringNames, event){
+  let message = []
+  for(let i=0; i<arrayStringNames.length; i++){
+    message.push(`Thank you, ${arrayStringNames[i]}, for the wonderful ${event} gift!`)
+  }
+  return message
+}
+
+function countDown(n){
+  let down = 10
+  while(down>= 0){
+  console.log(down--)
+  }
+  return down
+}
